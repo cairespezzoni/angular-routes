e7bc81c77e1e5eb7cac37e00a961ab1417d107a8
Caires Pezzoni <caires@smn.com.br>
2017-07-27 17:36:26 -0300

    Conclusão do exercício para aula.

--- locadora/src/app/app.module.ts
+++ locadora/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from 'app/app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,27 +18,15 @@ import { JogoService } from 'app/jogo/service/jogo.service';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    FilmeListaComponent,
     FilmeInfoComponent,
     JogoListaComponent,
     JogoInfoComponent,
-    FilmeListaComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-
-      { path: 'filme', component: FilmeListaComponent },
-      { path: 'filme/:id', component: FilmeInfoComponent },
-      { path: 'filme/novo', component: FilmeInfoComponent },
-
-      { path: 'jogo', component: JogoListaComponent },
-      { path: 'jogo/id', component: JogoInfoComponent },
-      { path: 'jogo/novo', component: JogoInfoComponent }
-    ])
   ],
   providers: [
     FilmeService,
--- locadora/src/app/filme/info/filme-info.component.ts
+++ locadora/src/app/filme/info/filme-info.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Location } from '@angular/common';
 
-import 'rxjs/add/operator/switchMap';
-
-import { FilmeService } from 'app/filme/service/filme.service'
 import { Filme } from 'app/filme/filme';
 
 @Component({
@@ -18,19 +13,12 @@ export class FilmeInfoComponent implements OnInit {
   @Input() filme: Filme;
 
   constructor(
-    private _filme: FilmeService,
-    private _route: ActivatedRoute,
-    private _location: Location
   ) { }
 
   ngOnInit(): void {
-    this._route.paramMap
-      .switchMap((params: ParamMap) => this._filme.getFilme(+params.get('id')))
-      .subscribe(filme => this.filme = filme);
   }
 
   voltar(): void {
-    this._location.back();
   }
 
 }
